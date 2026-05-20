31ad412c8489a0ca89669e4e8b36f6550a2f3947
Hari Hara Teja <hariharateja1977@gmail.com>
2026-05-20 16:51:24 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.cpp
@@ -0,0 +1,91 @@
+class Node{
+public:
+    int key,val;
+    Node* prev;
+    Node* next;
+
+    Node(int k,int v){
+        key=k;
+        val=v;
+        prev=nullptr;
+        next=nullptr;
+    }
+};
+
+class LRUCache {
+public:
+    int cap;
+    unordered_map<int,Node*> mp;
+
+    Node* head;
+    Node* tail;
+
+    LRUCache(int capacity) {
+        cap=capacity;
+
+        head=new Node(-1,-1);
+        tail=new Node(-1,-1);
+
+        head->next=tail;
+        tail->prev=head;
+    }
+
+    void remove(Node* node){
+        Node* p=node->prev;
+        Node* n=node->next;
+
+        p->next=n;
+        n->prev=p;
+    }
+
+    void insert(Node* node){
+        node->next=head->next;
+        node->prev=head;
+
+        head->next->prev=node;
+        head->next=node;
+    }
+
+    int get(int key) {
+
+        if(mp.find(key)==mp.end())
+            return -1;
+
+        Node* node=mp[key];
+
+        remove(node);
+        insert(node);
+
+        return node->val;
+    }
+
+    void put(int key, int value) {
+
+        if(mp.find(key)!=mp.end()){
+            Node* node=mp[key];
+
+            node->val=value;
+
+            remove(node);
+            insert(node);
+
+            return;
+        }
+
+        Node* node=new Node(key,value);
+
+        mp[key]=node;
+        insert(node);
+
+        if(mp.size()>cap){
+
+            Node* lru=tail->prev;
+
+            remove(lru);
+
+            mp.erase(lru->key);
+
+            delete lru;
+        }
+    }
+};
