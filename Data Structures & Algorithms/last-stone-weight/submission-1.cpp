6fd8ce0d37e835099843868832f3d08399515c29
Hari Hara Teja <hariharateja1977@gmail.com>
2026-05-23 16:07:09 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int num : stones){
+            pq.push(num);
+        }
+        while(pq.size()>1){
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            if(x!=y){
+                pq.push(x-y);
+            }
+        }
+        return pq.size()==1 ? pq.top() : 0;
+    }
+};
